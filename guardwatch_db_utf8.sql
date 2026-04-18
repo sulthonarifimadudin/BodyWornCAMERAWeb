e1326b8d8f8ef593288c9ba89f1c69fab5eb0397
Sulthon Arif Imadudin <119640272+sulthonarifimadudin@users.noreply.github.com>
2026-04-18 13:37:50 +0700

    fix(db): convert database dump to UTF-8 to prevent docker mysql fail import

---
+++ guardwatch_db_utf8.sql
@@ -0,0 +1,122 @@
+﻿-- MySQL dump 10.13  Distrib 8.4.3, for Win64 (x86_64)
+--
+-- Host: localhost    Database: guardwatch_db
+-- ------------------------------------------------------
+-- Server version	8.4.3
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `otp_codes`
+--
+
+DROP TABLE IF EXISTS `otp_codes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `otp_codes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `otp` varchar(6) DEFAULT NULL,
+  `expired_at` datetime DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `otp_codes_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `otp_codes`
+--
+
+LOCK TABLES `otp_codes` WRITE;
+/*!40000 ALTER TABLE `otp_codes` DISABLE KEYS */;
+INSERT INTO `otp_codes` VALUES (4,4,'700268','2026-04-14 13:00:25','2026-04-14 05:55:25');
+/*!40000 ALTER TABLE `otp_codes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `full_name` varchar(255) DEFAULT NULL,
+  `position` varchar(100) DEFAULT NULL,
+  `location` varchar(255) DEFAULT NULL,
+  `profile_image` varchar(255) DEFAULT NULL,
+  `is_online` tinyint(1) DEFAULT '0',
+  `last_seen` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'alfaiz1002@gmail.com','$2b$10$9K4Tzs0.M3kbzItrsQdPtu2gCjIw6A5JRt0Z.O806ZCSKy74ZpjI2','6281284229768','2026-04-11 23:56:09','alfaiz','Komandan Regu','Gedung A, Kampus Utama','1776275703642-foto-jibol.png',1,'2026-04-16 14:38:08'),(2,'Junior123@gmail.com','$2b$10$pJhIZV9k5Rom7XqL.qLHJu7S64aRhS5OodjUuD849c9ukNbvRPY5q','6281284229768','2026-04-12 01:07:02','junior','Komandan Regu','Gedung C, Kampus Utama',NULL,0,NULL),(3,'AgusSalim@gmail.com','$2b$10$4DzIa.Fb/Ce2UuPuBF4Rh.OhRoH3cp7SB7qt5oV36DqnWnXjKzNrW','6281284229768','2026-04-12 01:14:52','Agus Salim','Kepala Keamanan','TULT',NULL,0,NULL),(4,'jihannur@gmail123','$2b$10$TFdtOF..TI40ex7ULJiE9.9NouZez.e2N1UE2L1.yowVNUb1trSE2','6282346592873','2026-04-14 05:55:25','jihan','Petugas Security','tult',NULL,0,NULL),(5,'prabowo@gmail.com','$2b$10$mXhm1WadH1HSvjO0VZnr/O/S362SFE4mqFeylxHGvTUWo.e0Qqe9.','6282346592873','2026-04-14 05:55:45','jihan','Petugas Security','tult',NULL,0,NULL),(6,'yunas1002@gmail.com','$2b$10$TGwj2p8cHdCDNNc9GnRGO.mtyUREfujWkoi246RGRhpV5GMXko1E.','6281284229768','2026-04-14 06:00:27','yunas','Petugas Security','tult',NULL,0,NULL);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `videos`
+--
+
+DROP TABLE IF EXISTS `videos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `videos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `video_path` varchar(255) NOT NULL,
+  `status` enum('pending','approved','rejected') DEFAULT 'pending',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `approved_by` int DEFAULT NULL,
+  `approved_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `approved_by` (`approved_by`),
+  CONSTRAINT `videos_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `videos_ibfk_2` FOREIGN KEY (`approved_by`) REFERENCES `users` (`id`) ON DELETE SET NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `videos`
+--
+
+LOCK TABLES `videos` WRITE;
+/*!40000 ALTER TABLE `videos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `videos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-04-18 11:11:21
+
